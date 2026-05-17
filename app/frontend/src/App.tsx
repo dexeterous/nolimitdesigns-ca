278549285216566fd582afba32307f66560e1d4f
MGX <admin@mgx.dev>
2026-05-17 14:55:41 +0000

    Added ScrollToTop feature.

--- app/frontend/src/App.tsx
+++ app/frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { Toaster } from '@/components/ui/sonner';
 import { TooltipProvider } from '@/components/ui/tooltip';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import ScrollToTop from './components/ScrollToTop';
 import { AuthProvider } from './contexts/AuthContext';
 import Index from './pages/Index';
 import GraphicDesignServices from './pages/GraphicDesignServices';
@@ -36,6 +37,7 @@ const App = () => (
       <Toaster />
       <AuthProvider>
         <BrowserRouter>
+          <ScrollToTop />
           <Routes>
             <Route path="/" element={<Index />} />
             {/* New Agency Pages */}
